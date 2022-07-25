00d5c7e7f24c222e5a1498c1b92c08312cc88e34
ras812 <69597319+ras812@users.noreply.github.com>
2022-07-26 00:35:17 +0300

    del 4 line

--- WarGame/WarGame/Program.cs
+++ WarGame/WarGame/Program.cs
@@ -6,9 +6,6 @@ namespace WarGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello");
-            Console.WriteLine("12345");
             Console.WriteLine("11111");
         }
     }
