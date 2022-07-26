1b57c93a7319e38735bb03350948aeb69565f488
Barsiy <109915146+Barsiy@users.noreply.github.com>
2022-07-26 13:46:37 +0300

    add: something

--- HappyCats/HappyCats/Program.cs
+++ HappyCats/HappyCats/Program.cs
@@ -7,6 +7,10 @@ namespace HappyCats
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            int[] arr = new int[5];
+            for (int i = 0; i < arr.Length; i++)
+            { }
+            int a = 5;
         }
     }
 }
