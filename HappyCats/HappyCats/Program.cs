866f3ea8fafa0c317626353f904591727cdf7bab
Barsiy <109915146+Barsiy@users.noreply.github.com>
2022-07-26 13:43:05 +0300

    add happyCats

---
+++ HappyCats/HappyCats/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HappyCats
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
