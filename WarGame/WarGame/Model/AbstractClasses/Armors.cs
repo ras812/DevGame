d699cb39a7dae29bad4ef39a71d397539ac885e3
ras812 <69597319+ras812@users.noreply.github.com>
2022-08-04 01:36:15 +0300

    Revert "Revert "Dev""
    This reverts commit 38f2fe2c3ac857bf8b38450c90e63a5d1d2b9a91.

---
+++ WarGame/WarGame/Model/AbstractClasses/Armors.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WarGame
+{
+    public abstract class Armors
+    {
+    }
+}
---
+++ WarGame/WarGame/Model/Class/Weapon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WarGame
+{
+    public class Weapon : Weapons
+    {
+        public Weapon(): base()
+        {
+            
+        }
+
+        public Weapon(int x)
+        {
+
+        }
+    }
+}
