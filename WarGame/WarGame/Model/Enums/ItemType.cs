2a1ba0954bdac6c936381a53f1f5f9041525e12d
ras812 <69597319+ras812@users.noreply.github.com>
2022-08-06 01:11:17 +0300

    add enum

---
+++ WarGame/WarGame/Model/Enums/ItemType.cs
@@ -0,0 +1,5 @@
+﻿using System;
+namespace WarGame
+{
+	public enum Type: int { Light = 1, Medium = 2, Heavy = 3 };
+}
--- WarGame/WarGame/Model/Enums/Type.cs
+++
@@ -1,6 +0,0 @@
-﻿using System;
-namespace WarGame
-{
-	public enum Type: int { Light = 1, Medium = 2, Heavy = 3};
-	
-}
