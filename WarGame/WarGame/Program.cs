731c8e3df3dd8f816db8d58928e4d57f127a1929
Barsiy <109915146+Barsiy@users.noreply.github.com>
2022-08-06 01:13:31 +0300

    add

--- WarGame/WarGame/Program.cs
+++ WarGame/WarGame/Program.cs
@@ -7,12 +7,36 @@ namespace WarGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("WarGame");
-            Warrior obj = new Warrior();
-            Console.WriteLine("Victory!");
-            Weapon sword = new Weapon();
-            Weapon mace = new Weapon();
-            Console.WriteLine("TrimBim!");
+            string text = "цифры";
+            bool test = IsDiggit(text);
+            Console.WriteLine(test);
+
+            
+            
+            
+            //Console.WriteLine("WarGame");
+            //Warrior obj = new Warrior();
+            //Console.WriteLine("Victory!");
+            //Weapon sword = new Weapon();
+            //Weapon mace = new Weapon();
+            //Console.WriteLine("TrimBim!");
         }
+    
+       public static bool IsDiggit(string text)
+        {
+            bool test = true;
+
+            foreach (char x in text)
+            {
+              if(char.IsDigit(x))
+                test = false;
+                
+            }
+           return test;
+        }
+    
     }
+
+
+
 }
