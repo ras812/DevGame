ea02f7140badaa76bf8265b68232bf61f1e5678e
Barsiy <109915146+Barsiy@users.noreply.github.com>
2022-08-10 23:14:42 +0300

    ADD CONSTRUCTOR

--- WarGame/WarGame/Program.cs
+++ WarGame/WarGame/Program.cs
@@ -4,39 +4,41 @@ namespace WarGame
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args)    //ПОЧЕМУ-ТО dmgMin ВСЕГДА РАВНА 0!
         {
-            Weapon mase = new Weapon("Boulder", ItemType.Medium, -5, -6);
+            Weapon mase = new Weapon("Boulder1", ItemType.Medium, 5, 10);
             Console.WriteLine($"Name[{mase.Name}], ItemType[{mase.WeaponType}], " +
                               $"dmgMin[{mase.DamageMin}], dmgMax[{mase.DamageMax}]");
 
-            Weapon sword = new Weapon("Excalibur", ItemType.Heavy, 10, 5);
-            Console.WriteLine($"Name[{sword.Name}], ItemType[{sword.WeaponType}], " +
-                              $"dmgMin[{sword.DamageMin}], dmgMax[{sword.DamageMax}]");
-            
-            Weapon dagger = new Weapon("Kriss", ItemType.Light, 3, 7);
-            Console.WriteLine($"Name[{dagger.Name}], ItemType[{dagger.WeaponType}], " +
-                              $"dmgMin[{dagger.DamageMin}], dmgMax[{dagger.DamageMax}]");
+            //            Weapon sword = new Weapon("Excalibur", ItemType.Heavy, 10, 5);
+            //            Console.WriteLine($"Name[{sword.Name}], ItemType[{sword.WeaponType}], " +
+            //                              $"dmgMin[{sword.DamageMin}], dmgMax[{sword.DamageMax}]");
 
-            // ВОТ ТАКИМ ОБРАЗОМ МОЖНО ПРОТЕСТИРОВАТЬ КЛАСС ОРУЖИЕ
+            //            Weapon dagger = new Weapon("Kriss", ItemType.Light, 3, 7);
+            //            Console.WriteLine($"Name[{dagger.Name}], ItemType[{dagger.WeaponType}], " +
+            //                              $"dmgMin[{dagger.DamageMin}], dmgMax[{dagger.DamageMax}]");
 
-            Armor shield = new Armor("Shield", ItemType.Light, 5);
-            Console.WriteLine($"Название {shield.Name}, уровень защиты: {shield.ArmorType}, " +
-                              $"количество защиты: {shield.ArmorDefence}");
+            //            // ВОТ ТАКИМ ОБРАЗОМ МОЖНО ПРОТЕСТИРОВАТЬ КЛАСС ОРУЖИЕ
 
-            Armor plate = new Armor("P1ate", ItemType.Heavy, -5); //    КАК-ТО СТРАННО ПРОВЕРКА РАБОТАЕТ. МНЕ НЕ ОЧЕНЬ НРАВИТСЯ ПРОВЕРКА В КОНСТРУКТОРЕ
-            Console.WriteLine($"Название {plate.Name}, уровень защиты: {plate.ArmorType}, " +
-                              $"количество защиты: {plate.ArmorDefence}");
+            //            Armor shield = new Armor("Shield", ItemType.Light, 5);
+            //            Console.WriteLine($"Название {shield.Name}, уровень защиты: {shield.ArmorType}, " +
+            //                              $"количество защиты: {shield.ArmorDefence}");
+
+            //            Armor plate = new Armor("P1ate", ItemType.Heavy, -5); //    КАК-ТО СТРАННО ПРОВЕРКА РАБОТАЕТ. МНЕ НЕ ОЧЕНЬ НРАВИТСЯ ПРОВЕРКА В КОНСТРУКТОРЕ
+            //            Console.WriteLine($"Название {plate.Name}, уровень защиты: {plate.ArmorType}, " +
+            //                              $"количество защиты: {plate.ArmorDefence}");
 
 
-            Armor magicProtect = new Armor("MagicProtect", ItemType.Medium, 20);
-            Console.WriteLine($"Название {magicProtect.Name}, уровень защиты: {magicProtect.ArmorType}, " +
-                             $"количество защиты: {magicProtect.ArmorDefence}");
+            //            Armor magicProtect = new Armor("MagicProtect", ItemType.Medium, 20);
+            //            Console.WriteLine($"Название {magicProtect.Name}, уровень защиты: {magicProtect.ArmorType}, " +
+            //                             $"количество защиты: {magicProtect.ArmorDefence}");
 
-            //ПРОТЕСТИРОВАЛА КЛАСС ЗАЩИТА
+            //            //ПРОТЕСТИРОВАЛА КЛАСС ЗАЩИТА
+            //        }
+
+
+            //    }
+
+
         }
-
-
-    }
-
-}
+    }   }
