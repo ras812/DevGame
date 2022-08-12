b48051b3e1da8b90c317af201f0afde3400433bc
Barsiy <109915146+Barsiy@users.noreply.github.com>
2022-08-12 23:00:15 +0300

    ADD CONSTRACTOR

--- WarGame/WarGame/Model/Class/Weapon.cs
+++ WarGame/WarGame/Model/Class/Weapon.cs
@@ -5,81 +5,167 @@ namespace WarGame
     public class Weapon : Weapons
     {
         // Fields
-        private string _name;
-        private ItemType _weaponType;
-        private int _damageMin;
-        private int _damageMax;
+        private readonly string _name;
+        private readonly ItemType _weaponType;
+        private readonly int _damageMin;
+        private readonly int _damageMax;
 
         // Properties
         public override string Name
         {
             get { return _name; }
-            set
-            {
-                if (Handlers.IsString(value))
-                {
-                    _name = value;
-                }
-                else
-                {
-                    _name = "LETTERS ONLY!";
-                    throw new ArgumentException("STRING MUST CONTAINS ONLY LETTERS!");
-                }
-            }
+            //set
+            //{
+            //    if (Handlers.IsString(value))
+            //    {
+            //        _name = value;
+            //    }
+            //    else
+            //    {
+            //        _name = "LETTERS ONLY!";
+            //        throw new ArgumentException("STRING MUST CONTAINS ONLY LETTERS!");
+            //    }
+            //}
         }
 
         public override ItemType WeaponType
         {
             get { return _weaponType; }
-            set { _weaponType = value; } 
+            //set { _weaponType = value; } 
         }
-        
+
         public override int DamageMin
         {                                   // А ТУТ И НЕЛЬЗЯ ЭТОГО ДЕЛАТЬ!!!
             get { return _damageMin; }      // ВОТ ТУТ ОН НЕ ДАЕТ ПОСТАВИТЬ VALUE
-            set
-            {
-                if (value > 0)
-                {
-                    _damageMin = value;
-                }
-                else
-                {
-                    _damageMin = 1;
-                }
-            }
+            //set
+            //{
+            //    if (value > 0)
+            //    {
+            //        _damageMin = value;
+            //    }
+            //    else
+            //    {
+            //        _damageMin = 1;
+            //    }
+            //}
         }
 
         public override int DamageMax
         {
             get { return _damageMax; }
-            set
-            {
-                if (value > 0)
-                {
-                    _damageMax = value;
+            //set
+            //{
+            //    if (value > 0)
+            //    {
+            //        _damageMax = value;
 
-                    if (_damageMax <= _damageMin)
-                    {
-                        _damageMax = _damageMin;
-                    }
+            //        if (_damageMax <= _damageMin)
+            //        {
+            //            _damageMax = _damageMin;
+            //        }
+            //    }
+            //    else
+            //    {
+            //        _damageMax = 1;
+            //    }
+        }
+    
+
+    //Constructors
+    public Weapon(string name, ItemType itemType, int dmgMin, int dmgMax)
+    {
+        try
+            {
+                if(Handlers.IsString(name))
+                {
+               
+                    _name = name;
+                
                 }
-                else
+                        
+            else
+                {
+                    _name = "В имени могут быть только буквы";
+                    throw new ArgumentException("Letters only!");
+
+                }
+            
+             }
+            
+            catch(Exception ex)
+            {
+                Console.WriteLine($"[{ex}]"); 
+            }
+            
+            _weaponType = itemType; 
+
+            try
+            {
+                if(dmgMin > 0)
+                {
+                    _damageMin = dmgMin;
+                }
+            
+            else
+                {
+                    _damageMin = 1;
+                    throw new ArgumentException("Can't be less 0!");
+                }
+
+            }
+            
+             catch (Exception ex)
+            {
+                Console.WriteLine($"[{ex}]");
+            }
+            
+               try
+            {
+                if (dmgMax > 0)
+                {
+                    _damageMax = dmgMax;
+                }
+            
+            else
                 {
                     _damageMax = 1;
+                    throw new ArgumentException("Can't be less 0!");
                 }
+            
             }
+        
+            catch(ArgumentException ex)
+            {
+                Console.WriteLine($"[{ex}]");
+            }
+            
+            try
+            {
+                if(dmgMin < dmgMax) 
+                {
+                    _damageMax = dmgMax;
+                    _damageMin = dmgMin;
+                }
+              else
+                {
+                    _damageMax = 2;
+                    _damageMin = 2;
+                    throw new ArgumentException($"{_damageMin} can't be more then {_damageMax}");
+                }
+            
+            
+            }
+             catch(ArgumentException ex)
+            {
+                Console.WriteLine($"[{ex}]");
+            }
+        
+        
         }
 
-        //Constructors
-        public Weapon(string name, ItemType itemType, int dmgMin, int dmgMax)
-        {
-            Name = name;
-            WeaponType = itemType;
-            DamageMin = dmgMin;
-            DamageMax = dmgMax;
-        }
+    
 
-        // Это написано тестово, тут будет написан ТВОЙ код )))
-    }
+}
+
+
 }
--- WarGame/WarGame/Program.cs
+++ WarGame/WarGame/Program.cs
@@ -4,22 +4,22 @@ namespace WarGame
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args)    //ПОЧЕМУ-ТО НЕ РАБОТАЕТ КОНСТРУКТОР ДЛЯ Weapon И Armor !
         {
             Weapon sword = new Weapon("Excalibur", ItemType.Heavy, 3, 7);
             Armor shield = new Armor("Shield", ItemType.Light, 5);
 
-            Warrior player1 = new Warrior();
-            player1.Display();
+            //    Warrior player1 = new Warrior();
+            //    player1.Display();
 
-            Warrior player2 = new Warrior("Jack", Gender.Male, 100, 6, 6, 6, sword, shield);
-            player2.Display();
+            //    Warrior player2 = new Warrior("Jack", Gender.Male, 100, 6, 6, 6, sword, shield);
+            //    player2.Display();
 
-            Arena arena = new Arena();
-            Console.WriteLine($"Vitorious is {arena.Attack(player1, player2).Name}!!!");
+            //    Arena arena = new Arena();
+            //    Console.WriteLine($"Vitorious is {arena.Attack(player1, player2).Name}!!!");
+            //}
+
+
         }
-
-
     }
-
 }
