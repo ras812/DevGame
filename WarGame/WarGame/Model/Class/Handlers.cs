9895bf26c100abc80ef4557e4eb5805e0d46afa4
Barsiy <109915146+Barsiy@users.noreply.github.com>
2022-08-21 23:23:01 +0300

    add ReadFiles

--- WarGame/WarGame/Model/Class/Handlers.cs
+++ WarGame/WarGame/Model/Class/Handlers.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace WarGame
 {
@@ -18,5 +19,25 @@ namespace WarGame
             }
             return b;
         }
+    
+      public static void ReadFiles(string fileName, int[] arr) //ФУНКЦИЯ КОТОРАЯ ЧИТАЕТ ФАЙЛ
+        {
+        BinaryReader br = new BinaryReader(File.Open(fileName, FileMode.Open));
+        int len = br.ReadInt32();   
+        arr = new int[len];
+            for (int c = 0; c < len; c++)
+            {
+                arr[c] = br.ReadInt32();
+            }
+        br.Close();
+
+            foreach (var v in arr)
+            {
+                Console.WriteLine(v + "  ");
+            }
+        
+        }
+    
+    
     }
 }
