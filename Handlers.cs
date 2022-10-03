b899e7a3239250b714c2667ad079f71b96dbcb07
Barsiy <109915146+Barsiy@users.noreply.github.com>
2022-10-03 22:26:31 +0300

    ADD

---
+++ Handlers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp12
+{
+    internal class Handlers
+    {
+        public static int ReadNum()
+        {
+            return int.Parse(Console.ReadLine());
+
+        }
+
+        
+    
+    }
+}
---
+++ MainMenu.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static ConsoleApp12.Program;
+
+namespace ConsoleApp12
+{
+   public class MainMenu
+    {
+        public MainMenu()
+        {
+                
+        }
+        public void DisplayMainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine($"Select type info:\n" +
+                                              $"================\n" +
+                                              $"[1] - [ Общая информация по типам ]\n" +
+                                              $"[2] - [ Выбрать тип из списка ]\n" +
+                                              $"[3] - [ Параменты консоли ]\n" +
+                                              $"[0] - [ Выход из программы ]\n"
+
+                                             );
+        }
+    
+    
+       public void StartMainMenu()
+        {
+            DisplayMainMenu();
+
+            while(true)
+            {
+                int ret = Handlers.ReadNum();
+                if(ret == 1)
+                {
+
+                }
+            
+                if(ret == 2)
+                {
+                    SelectTypeInfo appStart = new SelectTypeInfo();
+                    appStart.StartSelectTypeInfo();
+                    break;
+                }
+
+                if (ret == 3)
+                {
+                    SelectColor s = new SelectColor();
+                    s.DisplayColor();
+                    break;
+                }
+                 if(ret == 0)
+                {
+                    Environment.Exit(0);
+                }
+
+            }
+        }
+    
+    }
+}
