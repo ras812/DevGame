56b92a48011ae2ed913b4b2f7605725fb7cb3bd6
ras812 <ras812812@gmail.com>
2022-10-03 13:39:06 -0700

    Update Uncommon.cs

--- WellcomeToLinq/WellcomeToLinq/Uncommon.cs
+++ WellcomeToLinq/WellcomeToLinq/Uncommon.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace WellcomeToLinq
 {
@@ -30,7 +31,35 @@ namespace WellcomeToLinq
             return charsList.ToArray();
         }
 
-        public static string[] 
+        public static string[] GetWordsFromFiles(string filesPath)
+        {
+            string[] fileNames = Directory.GetFiles(filesPath);
+            List<string> linesList = new List<string>();
+
+            foreach (var file in fileNames)
+            {
+                string[] lines = File.ReadAllLines(file);
+                linesList.AddRange(lines);
+            }
+
+            List<string> wordsList = new List<string>();
+            char[] delimiterChars = GetDelimiterChars();
+
+            foreach (var item in linesList)
+            {
+                string[] sub = item.Split(delimiterChars,
+                                          StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var s in sub)
+                {
+                    s.Trim();
+                }
+
+                wordsList.AddRange(sub);
+            }
+
+            return wordsList.ToArray();
+        }
     }
 }
 
