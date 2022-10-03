135988ef6563db1ca95c56217ac61181fd6c727d
ras812 <ras812812@gmail.com>
2022-10-03 13:00:13 -0700

    Update Uncommon.cs

--- WellcomeToLinq/WellcomeToLinq/Uncommon.cs
+++ WellcomeToLinq/WellcomeToLinq/Uncommon.cs
@@ -1,9 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace WellcomeToLinq
 {
-	public class Uncommon
-	{
-		//public static 
-	}
+    public class Uncommon
+    {
+        public static char[] GetDelimiterChars()
+        {
+            List<char> charsList = new List<char>();
+
+            int charMin = char.MinValue;
+            int charMax = char.MaxValue;
+
+            for (int i = charMin; i <= charMax; i++)
+            {
+                int num = i;
+                char c = (char)num;
+                if (char.IsSeparator(c) ||
+                    char.IsPunctuation(c) ||
+                    char.IsWhiteSpace(c) ||
+                    char.IsControl(c) ||
+                    char.IsSymbol(c)
+                   )
+                {
+                    charsList.Add(c);
+                }
+            }
+
+            return charsList.ToArray();
+        }
+
+        //public static 
+    }
 }
 
